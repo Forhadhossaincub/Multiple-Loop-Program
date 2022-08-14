6f581d3830b046d93a3e911f814b5e904b1556df
FORHAD HOSSAIN <forhadhossaincub@gmail.com>
2022-08-14 11:16:12 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Loop_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for(int i=0,j=10; i<j; i++, j--)
+            {
+                Console.WriteLine($"i and j {i} , {j}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
